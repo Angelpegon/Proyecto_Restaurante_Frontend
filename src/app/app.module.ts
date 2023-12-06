7448da821cb5b4b8678b051bac9c466ecfa98107
Angelpegon <tecangelesteban@gmail.com>
2023-12-05 23:28:59 -0500

    Actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { SharedModule } from './components/shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/dashboard/mesero/crearmesero/crearmesero.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MeseroService } from 'src/app/services/mesero/mesero.service';
+
+@Component({
+  selector: 'app-crearmesero',
+  templateUrl: './crearmesero.component.html',
+  styleUrls: ['./crearmesero.component.css']
+})
+export class CrearmeseroComponent implements OnInit {
+  personaForm!: FormGroup;
+  constructor(
+    public fb: FormBuilder,
+    public meserosService: MeseroService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.personaForm = this.fb.group({
+      id: [''],
+      nombre: ['', Validators.required],
+    })
+  }
+  guardar(): void {
+    this.meserosService.saveMesero(this.personaForm.value).subscribe(resp => {
+      this.personaForm.reset();
+      this.personaForm.setErrors(null);
+    },
+      error => { console.error(error) }
+    )
+  }
+}
